2f8ac30aeabdc3aa5f69b0fbd363889e3f8ef699
Leo <keyyk220@gmail.com>
2026-05-06 21:16:05 +0700

    v1

---
+++ tugas-1.py
@@ -0,0 +1,41 @@
+#integer
+umur = 16
+jumlah_siswa = 30
+
+#float / desimal
+tinggi_badan = 165.5
+nilai_rata_rata = 85.7
+
+#string / teks
+nama = "Budi"
+sekolah = "SMK Analis Kimia YKPI Bogor"
+
+#Boolean (True/False)
+sudah_lulus = False
+sedang_belajar = True
+
+#fungsi tipe data
+angka = 42
+pecahan = 3.14
+kata = "Python"
+benar = True
+
+#menampilkan fungsi Variable dan data lainnya
+print(f"nama saya", nama)
+print(f"Umur", umur)
+print(f"Tinggi Badan", tinggi_badan)
+print(f"Sudah lulus", sudah_lulus)
+
+#fungsi tipe
+print(type(nama))
+print(type(umur))
+print(type(kata))
+print(type(sudah_lulus))
+
+#jika digabungkan
+
+print("nama: ", nama, "tipe: ", type(nama))
+print("umur: ", umur, "tipe: ", type(umur))
+print("kata: ", kata, "tipe: ", type(kata))
+print("sudah lulus: ", sudah_lulus, "tipe: ", type(sudah_lulus))
+
---
+++ tugas-2.py
@@ -0,0 +1,27 @@
+#tugas mandiri
+
+nama = "John"
+umur = 17
+tinggi = 170.5
+masih_sekolah = True
+
+#panggil data
+
+print("="*30, "DATA-MANDIRI", "="*30)
+print(f"nama: ", nama)
+print(f"umur: ", umur)
+print(f"tinggi: ", tinggi)
+print(f"Masih Sekolah: ", masih_sekolah)
+
+print("="*30, "tipe data", "="*30)
+print("tipe nama: ", type(nama))
+print("tipe umur: ", type(umur))
+print("tipe tinggi: ", type(tinggi))
+print("tipe masih sekolah: ", type(masih_sekolah))
+
+print("="*30, "digabung", "="*30)
+print(f"nama: ", nama, "tipe: ", type(nama))
+print(f"umur: ", umur, "tipe: ", type(umur))
+print(f"tinggi: ", tinggi, "tipe: ", type(tinggi))
+print(f"Masih Sekolah: ", masih_sekolah, "tipe: ", type(masih_sekolah))
+
