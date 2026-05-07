fffa0fabbacc385c561fe8303a1aa6096c24d7ef
Leo <keyyk220@gmail.com>
2026-05-07 14:20:19 +0700

    Add files via upload

---
+++ tugas-3.py
@@ -0,0 +1,42 @@
+foo = 'SMKAKAYKPI'
+print(foo)
+
+bar = 'SMKAKAYKPI'
+print(bar)
+
+print("halo \t hai")
+
+#baris
+foo2 = "teks ini akan dipecah \n kedalam 2 baris"
+print(foo2)
+
+foo3 = "teks ini \n akan dipecah \n kedalam 3 baris"
+print(foo3)
+
+xxz = """aku
+cpp
+hehehe"""
+print(xxz)
+
+#penyambungan
+cxx = 'Belajar'
+py = ' Bahasa pemograman CPP'
+kt = ' di sekolah'
+gabung = cxx + py + kt
+print(gabung)
+
+#ex 
+ac = ('sekolahan')
+print(ac[0])
+print(ac[4])
+print(ac[5:7])
+
+high_level = ['html','css', 'js']
+print(high_level.append('kt'))
+#print
+print(high_level)
+
+low_end = ['cpp','java', 'c', 'asm']
+
+#menghapus
+print(low_end.pop(3))
