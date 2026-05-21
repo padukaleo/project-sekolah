517e16c9ce224c29fc6666773a984c79be32b43f
Leo <keyyk220@gmail.com>
2026-05-21 14:14:28 +0700

    uprak: V1

---
+++ tugas-4.py
@@ -0,0 +1,35 @@
+baris = "SMK Analis Kimia YKPI Bogor \n menciptakan \n lulusan \n yang \n siap \n kerja"
+print(baris)
+
+sambung_1 = "SMK "
+sambung_2 = "Analis "
+sambung_3 = "Kimia "
+sambung_4 = "YKPI Bogor "
+sambung_5 = "Tetap "
+sambung_6 = "Jaya "
+sambung_7 = "Selama "
+sambung_8 = "lamanya "
+
+sambung = sambung_1 + sambung_2 + sambung_3 + sambung_4 + sambung_5 + sambung_6 + sambung_7 + sambung_8
+print(sambung)
+
+list_data = ["2", "4", "6", "9", "11"]
+nama = "sekolah menengah kejuruan"
+print("array dari 2,4,6,9,11 : ")
+print("nama: ",nama[2])
+print("nama: ",nama[4])
+print("nama: ",nama[6])
+print("nama: ",nama[9])
+print("nama: ",nama[11])
+
+nama_2 = "SMK Analis Kimia YKPI Bogor"
+print(nama_2)
+print("nama: ",nama_2[0])
+print("nama: ",nama_2[2])
+
+list_data_2 = ["kelas_X", "kelas_XI", "kelas_XII"]
+print(list_data_2)
+print(list_data_2.append("kelas_XIII"))
+print(list_data_2)
+print(list_data_2.pop(2))
+
